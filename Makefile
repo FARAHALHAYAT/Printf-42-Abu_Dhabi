06d7dcda71262c56548a0e694ad484bdf7eb7be1
fasharif <farahshraif@gmail.com>
2022-08-14 15:11:09 +0400

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,28 +1,30 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: fasharif <fasharif@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/08/12 17:46:27 by fasharif          #+#    #+#              #
+#    Updated: 2022/08/12 17:56:17 by fasharif         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 NAME = libftprintf.a
 cc = gcc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror 
 
-SRCS = \
-	  ft_printf.c \
-	  ft_putchar.c \
-	  ft_putstr.c \
-	  ft_itoa.c \
-	  ft_puthex.c
+SRCS = ft_printf.c ft_putchar.c ft_putstr.c ft_itoa.c ft_puthex.c
 
 OBJS = $(SRCS:.c=.o)
 
-all: $(NAME)
-
-$(NAME): $(OBJS)
-
-%.o: %.c
-	$(CC) $(CFLAGS) -c -o $@ $^
-	ar crsu $(NAME) $@
-
+.c.o:
+		${CC} ${CFLAGS} -g -c $< -o ${<:.c=.o}
+$(NAME): ${OBJS}
+		ar rcs ${NAME} ${OBJS}
+all:	${NAME}
 clean:
-	rm -f $(OBJS)
-
-fclean: clean
-	rm -f $(NAME)
-
-re: clean all
+		${RM} ${OBJS} ${OBJSBONUS}
+fclean:	clean
+		${RM} ${NAME}
+re:		fclean all
--- ft_itoa.c
+++ ft_itoa.c
@@ -1,40 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fasharif <fasharif@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/12 15:23:40 by fasharif          #+#    #+#             */
+/*   Updated: 2022/08/12 16:49:50 by fasharif         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-static long	countdigit(long n)
+static long	countdigit(long nb)
 {
 	int	i;
 
-	i = 1;
-	while (n >= 9)
+	i = 0;
+	while (nb > 9)
 	{
-		n = n / 10;
+		nb /= 10;
 		i++;
 	}
 	return (i);
 }
 
-static void	putnbr(long n)
+static void	putnbr(long nbr)
 {
-	if (n > 9)
-		putnbr(n / 10);
-	ft_putchar(n % 10 + '0');
+	if (nbr >= 9)
+		putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + '0');
 }
 
 int	ft_itoa(int n)
 {
-	long	l;
+	long	i;
 	int		size;
 
 	size = 0;
-	l = n;
-	if (l < 0)
+	i = n;
+	if (i < 0)
 	{
 		ft_putchar('-');
-		l *= -1;
+		i *= -1;
 		size += 1;
 	}
-	size += countdigit(l);
-	putnbr(l);
+	size += countdigit(i);
+	putnbr(i);
 	return (size);
 }
 
