a18217adefb9b98d11d06152cd9d7ad05b9b2002
fasharif <farahshraif@gmail.com>
2022-08-15 14:12:53 +0400

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fasharif <fasharif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 17:58:34 by fasharif          #+#    #+#             */
-/*   Updated: 2022/08/12 17:58:43 by fasharif         ###   ########.fr       */
+/*   Updated: 2022/08/15 14:11:35 by fasharif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int	main(void)
 	printf("%u \n", 42);
 	printf("%x %X \n", 42, 42);
 	printf("%% \n\n");
-
 	ft_printf("%c \n", 'a');
 	ft_printf("%s \n", "abc");
 	ft_printf("%p \n", (void *)42);
@@ -29,5 +28,5 @@ int	main(void)
 	ft_printf("%u \n", 42);
 	ft_printf("%x %X \n", 42, 42);
 	ft_printf("%% \n");
-	return 0;
+	return (0);
 }
